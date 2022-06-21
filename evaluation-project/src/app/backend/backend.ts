7ba34316af9e8def27a1b597b43e345aff83a8dc
VIKAS TIWARI <69078837+vikastiwari111@users.noreply.github.com>
2022-06-21 11:59:58 +0530

    Delete evaluation-project/src/app/backend directory

--- evaluation-project/src/app/backend/backend.ts
+++
@@ -1,9 +0,0 @@
-
-import { ILogin } from "src/app/app.types";
-export const backendStudentList = async (): Promise<ILogin[]> => {
-    return [
-        { email: 'vikas123@gmail.com', password: 12345 },
-        {email: 'sagar123@gmail.com', password: 123321  },
-        {email: 'abhilash123@gmail.com', password: 345678  }
-    ];
-}
